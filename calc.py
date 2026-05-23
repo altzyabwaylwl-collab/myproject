245a6dd4a3a4f4a3713074da20c985f6eb8efd62
Abood <altzyabwaylwl@gmail.com>
2026-05-23 15:52:55 +0300

    add calculator only

---
+++ calc.py
@@ -0,0 +1,6 @@
+# كود بايثون لحساب مجموع عددين
+num1 = float(input("أدخل العدد الأول: "))
+num2 = float(input("أدخل العدد الثاني: "))
+
+sum_result = num1 + num2
+print(f"مجموع العددين هو: {sum_result}")
